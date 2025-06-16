0a71d3cb96d7d6ada92ed74e4eab2fe89a4136a7
岡庭優也 <yucyanzz77@gmail.com>
2025-06-16 17:07:33 +0900

    web

--- main.py
+++ main.py
@@ -1,10 +1,8 @@
 from typing import Optional
 
-from fastapi import FastAPI
-
-from fastapi.responses import HTMLResponse #インポート
-
-import random  # randomライブラリを追加
+from fastapi import FastAPI, Query
+from fastapi.responses import HTMLResponse
+import random
 
 app = FastAPI()
 
@@ -31,26 +29,22 @@ def omikuji():
         "小凶",
         "大凶"
     ]
-    
-    return {"result" : omikuji_list[random.randrange(10)]}
-
-
-### コードいろいろ... ###
+    return {"result": random.choice(omikuji_list)}
 
 @app.get("/index")
 def index():
     html_content = """
     <html>
         <head>
-            <title>Some HTML in here</title>
+            <title>FastAPI Index</title>
         </head>
         <body>
-            <h1>Look ma! HTML!</h1>
+            <h1>Welcome to the FastAPI Index Page!</h1>
         </body>
     </html>
     """
     return HTMLResponse(content=html_content, status_code=200)
 
-    @app.post("/present")
-async def give_present(present):
-    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}  # f文字列というPythonの機能を使っている
+@app.post("/present")
+async def give_present(present: str = Query(...)):
+    return {"response": f"サーバです。メリークリスマス！ {present}ありがとう。お返しはキャンディーです。"}
